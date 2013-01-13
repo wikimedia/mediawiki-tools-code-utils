b1e3ac6af225c48a5bb61d660a7e177ebe92bd29
Antoine Musso <hashar@free.fr>
2013-01-13 20:43:01 +0100

    read_wfMsgCalls.py wrap long line, kill w/s
    Passing pep8 on the read_wfMsgCalls raises a lot of issues. This patch:
    * removes the trailing whitespaces
    * kill multiple blank lines
    * rewrap overlong comments
    
    Change-Id: I5d0ffbc552a9f2f519eb3ad71d8a839f2b941cdf

--- read_wfMsgCalls.py
+++ read_wfMsgCalls.py
@@ -3,25 +3,41 @@
 
 #
 # Two small tools to analyse the keys of the i18n messages of MediaWiki
-# 1/ Get the current keys (and corresponding language and message) from their definition
-# 2/ Get the calls of the wfMsg functions (and corresponding file, line, type (wfMsg, wfMsgForContent, etc.), message key (when possible))
-# 
-# 
+# 1/ Get the current keys (and corresponding language and message) from their
+#    definition
+# 2/ Get the calls of the wfMsg functions (and corresponding file, line, type
+#    (wfMsg, wfMsgForContent, etc.), message key (when possible))
+#
+#
 # Some details:
 # * this program calls a PHP interpreter, so you need it
-# * the occurences of wfMsg functions in block comments are removed (about 30 occurences), I didn’t check for single-line comments
-# * I assumed the key of the messages are '([a-zA-Z0-9_-]+?)', it seems quite correct after tests (anyway no wfMsg call is forgotten, even if the key is not recognized)
-# * hence the key with a variable are never computed since you must have a deeper program analysis, the cases are wfMsg( $wgLogNames[$type] ) or (easier?) wfMessage( 'block-log-flags-' . $flag )
-# * some calls are missed when called by call_user_func_array, but in these case you probably have no chance to get the associated message key because it is probably a variable)
+# * the occurences of wfMsg functions in block comments are removed (about 30
+#   occurences), I didn’t check for single-line comments
+# * I assumed the key of the messages are '([a-zA-Z0-9_-]+?)', it seems quite
+#   correct after tests (anyway no wfMsg call is forgotten, even if the key is
+#   not recognized)
+# * hence the key with a variable are never computed since you must have a
+#   deeper program analysis, the cases are wfMsg( $wgLogNames[$type] ) or
+#   (easier?) wfMessage( 'block-log-flags-' . $flag )
+# * some calls are missed when called by call_user_func_array, but in these
+#   case you probably have no chance to get the associated message key because
+#   it is probably a variable)
 # * the results are CSV
-# * the format of the messageStrings file is: 1/ language code (from the name of the file when available); 2/ message key; 3/ content of the message
-# * the format of the wfMsgCalls file is: 1/ path of the file; 2/ line number; 3/ wfMsg type (wfMsg, wfMessage, etc.); 4/ message key (when possible); 5/ complete call of the function
-# 
+# * the format of the messageStrings file is:
+#     1/ language code (from the name of the file when available);
+#     2/ message key;
+#     3/ content of the message
+# * the format of the wfMsgCalls file is:
+#     1/ path of the file;
+#     2/ line number;
+#     3/ wfMsg type (wfMsg, wfMessage, etc.);
+#     4/ message key (when possible);
+#     5/ complete call of the function
+#
 # Author: Seb35 on Wikimedia projects
-# 
+#
 # License: WTFPL 2.0 http://sam.zoy.org/wtfpl/
 #
-
 
 
 # # # # # # # #
@@ -33,10 +49,14 @@
 # Folder containing a tree of MediaWiki
 baseFolder = "mediawiki/trunk/phase3"
 
-# Name of the CSV result file (in the current folder) containing the calls to the functions wfMsg* (specified thereafter in a parameter) obtained by analysing the code
+# Name of the CSV result file (in the current folder) containing the calls to
+# the functions wfMsg* (specified thereafter in a parameter) obtained by
+# analysing the code
 wfMsgCallsResultFile = "wfMsgCalls.csv"
 
-# Name of the CSV result file (in the current folder) containing the associations lang-msgkey-message by retrieving the content of the PHP $messages variable in the 'languages' and i18n folders
+# Name of the CSV result file (in the current folder) containing the
+# associations lang-msgkey-message by retrieving the content of the PHP
+# $messages variable in the 'languages' and i18n folders
 messageStringsResultFile = "messageStrings.csv"
 
 # Save also the content of the messages (count 10Mio without and 30Mio with)
@@ -54,7 +74,8 @@ messagesFolders = { 'phase3': [ 'languages/messages' ], 'extensions': [  ], '#':
 # Exclude these files
 messagesExcludeFiles = []
 
-# Include these files (must not be in the previous folders else it would be duplicated)
+# Include these files (must not be in the previous folders else it would be
+# duplicated)
 messagesIndividualFiles = []
 
 
@@ -66,10 +87,9 @@ codeFolders = [ "includes", "extensions", "skins", "languages/classes", "#" ]
 # Exclude these files
 codeExcludeFiles = []
 
-# Include these files (must not be in the previous folders else it would be duplicated)
+# Include these files (must not be in the previous folders else it would be
+# duplicated)
 codeIndividualFiles = [ "languages/Language.php", "languages/LanguageConverter.php", "languages/Names.php", "resources/Resources.php" ]
-
-
 
 
 # # # # # # # # # # # #
@@ -78,7 +98,6 @@ codeIndividualFiles = [ "languages/Language.php", "languages/LanguageConverter.p
 
 import os, os.path, re, csv, subprocess
 
-
 currentFolder = os.getcwd()
 os.chdir( baseFolder )
 
@@ -86,7 +105,7 @@ i18nMessages = []
 
 # Iterate over folders and files
 for messagesFolderType in messagesFolders:
-	
+
 	directories = []
 	if messagesFolderType == 'extensions':
 		for directory in messagesFolders[messagesFolderType]:
@@ -96,29 +115,29 @@ for messagesFolderType in messagesFolders:
 					continue
 				directories.append( j[0] )
 		messagesFolders[messagesFolderType] = directories
-	
+
 	for messagesFolder in messagesFolders[messagesFolderType]:
-		
+
 		if messagesFolderType != '#':
 			files = os.listdir( messagesFolder )
 		else:
 			files = messagesIndividualFiles
 			messageFolder = ''
-		
+
 		for filename in files:
-			
+
 			if filename[-4:] != '.php':
 				continue
-			
+
 			if messagesFolderType == 'extensions' and filename[-9:] != '.i18n.php':
 				continue
-			
+
 			if filename in messagesExcludeFiles:
 				continue
-			
+
 			if messagesFolderType == 'phase3' and filename[:8] == 'Messages':
 				lang = filename[8:-4]
-			
+
 			# Read the PHP $messages variable
 			p = subprocess.Popen( 'php', shell=True, stdin=subprocess.PIPE, stdout=subprocess.PIPE, close_fds=True )
 			print >>p.stdin, '<?php'
@@ -138,19 +157,19 @@ for messagesFolderType in messagesFolders:
 				}
 			"""
 			p.stdin.close()
-			
+
 			messages = str.splitlines( p.stdout.read() )
-			
+
 			if len(messages) == 0:
 				if messagesFolderType == 'phase3' and filename[:8] == 'Messages':
 					print 'Core language '+lang+' doesn’t have a $message variable in '+filename+' or is empty.'
 				else:
 					print 'File '+filename+' doesn’t have a $message variable or is empty.'
 				continue
-			
+
 			# Retrieve the result and put it in a list of list
 			for message in messages:
-				
+
 				msg = message.split( ' ', 1 )
 				i18nMessage = []
 				if '|' in msg[0]:
@@ -183,14 +202,14 @@ msgFunctionsMaxLength = max( [ len(f) for f in messageFunctions ] )
 
 # Iterate over folders and files
 for folder in codeFolders:
-	
+
 	if folder != '#':
 		directories = os.walk( folder )
 	else:
 		directories = [ '.' ]
-	
+
 	for directory in directories:
-		
+
 		if folder != '#':
 			if '.svn' in directory[0]:
 				continue
@@ -199,20 +218,21 @@ for folder in codeFolders:
 		else:
 			fileset = codeIndividualFiles
 			direct = ''
-		
+
 		for filename in fileset:
-			
+
 			if filename[-4:] != ".php":
 				continue
-			
+
 			if filename in codeExcludeFiles:
 				continue
-			
+
 			fyle = open( os.path.join( direct, filename ), 'r' )
-			
+
 			content = fyle.read()
-			
-			# Remove the false positive in block comments (some could remain if in single-line comments)
+
+			# Remove the false positive in block comments (some could remain if
+			# in single-line comments)
 			incomment = False
 			for c in range(len(content)-1):
 				if c == len(content):
@@ -225,22 +245,23 @@ for folder in codeFolders:
 					f = re.search( '^'+msgFunctionsSoft, content[c:c+msgFunctionsMaxLength] )
 					if f != None:
 						content = content[:c] + content[c+f.end():]
-			
-			# Get the indexes of the beginning of lines (to compute after the line number)
+
+			# Get the indexes of the beginning of lines (to compute after the
+			# line number)
 			indexOfBeginningOfLines = [0]
 			for m in re.finditer( '(?:\n|\r|\n\r|\r\n)', content ):
 				indexOfBeginningOfLines.append( m.end() )
-				
+
 			if indexOfBeginningOfLines[-1] != len(content):
 				indexOfBeginningOfLines.append( len(content) )
-			
+
 			# Iterate to find the wfMsg functions
 			for m in re.finditer( msgFunctions, content, re.S ):
-				
+
 				i = -1
 				while m.start()-indexOfBeginningOfLines[i] < 0:
 					i = i - 1
-				
+
 				# Search the key once we recognized the message
 				key = ''
 				k = re.search( "^\(\s*'([a-zA-Z0-9_-]+?)'\s*(?:,|\))", m.group(2) )
@@ -250,36 +271,36 @@ for folder in codeFolders:
 					k = re.search( '^\(\s*"([a-zA-Z0-9_-]+?)"\s*(?:,|\))', m.group(2) )
 					if k != None:
 						key = k.group(1)
-				
+
 				wfMsgCall = [ os.path.join( directory[0], filename ), len(indexOfBeginningOfLines)+i+1, m.group(1), key, m.group(0) ]
-				
-				# You must have the same number of opening and closing parenthesis
+
+				# You must have the same number of opening and closing
+				# parenthesis
 				if m.group(0).count( '(' ) > 1:
-					
+
 					recursion = 0
 					pos = m.end()
-					
+
 					while wfMsgCall[4].count( '(' ) != wfMsgCall[4].count( ')' ):
-						
+
 						endparenthesis = ''
 						for nbparenthesis in range( wfMsgCall[4].count( '(' ) - wfMsgCall[4].count( ')' ) ):
 							endparenthesis = endparenthesis + '.*?\)'
 						res = re.search( endparenthesis, content[pos:], re.S )
-						
+
 						pos = pos + res.end()
-						
+
 						if res != None:
 							wfMsgCall[4] = wfMsgCall[4] + res.group(0)
 						else:
 							raise Exception( 'parenthesis expected' )
 						recursion = recursion + 1
-						
+
 						if recursion == 10:
 							raise Exception( 'recursion' )
-				
+
 				wfMsgCalls.append( wfMsgCall )
 
 os.chdir( currentFolder )
 writer = csv.writer( open( wfMsgCallsResultFile, 'w' ) )
 writer.writerows( wfMsgCalls )
-
