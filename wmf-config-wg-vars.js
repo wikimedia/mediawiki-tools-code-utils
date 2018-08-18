be26954d30c4f2fdc52b5fc2484e817d56bce6e1
Timo Tijhof <krinklemail@gmail.com>
2018-08-18 22:51:30 +0100

    Update wmf-config-wg-vars.js
    Import revision 2/2
    from <https://gist.github.com/Krinkle/15be368abdbf918c8ef7a06cde011457>.
    
    Change-Id: I94bf48fb474de8f7b91bb5350245fdadcfafc3e5

--- wmf-config-wg-vars.js
+++ wmf-config-wg-vars.js
@@ -2,23 +2,26 @@ const fs = require('fs');
 const req = require('request-promise-native');
 const querystring = require('querystring');
 
+// ~/Development/wikimedia/operations/mediawiki-config (master)
+// $ git grep "[$'\"]wg[A-Z]"  > ~/Development/tmp/wmf-config-wg-vars.log
 const lines = fs.readFileSync('./wmf-config-wg-vars.log').toString().split('\n');
 const varNames = Object.create(null);
 
 // Collect variable names
 lines.forEach( line => {
-	const fileName = line.split(':', 1)[0];
+	// Abbreviate the common case of 'wmf-config'
+	const fileName = line.split(':', 1)[0].replace(/^wmf-config\//, '');
 	if (fileName.slice(-4) !== '.php') {
 		// Ignore .html, .xml etc.
 		return;
 	}
 	const rVar = /[$'"]wg([A-Za-z0-9_]+)/g;
 	var match;
-	while ( (match = rVar.exec(line)) !== null ) {
-		if ( !( match[1] in varNames ) ) {
+	while ((match = rVar.exec(line)) !== null) {
+		if (!(match[1] in varNames)) {
 			varNames[ match[1] ] = new Set()
 		}
-		varNames[ match[1] ].add( fileName );
+		varNames[ match[1] ].add(fileName);
 	}
 } );
 // Clear
@@ -55,7 +58,21 @@ console.log('Found ' + Object.keys(varNames).length + ' unique wg* variable name
 		}
 	}
 
-	// Dump vars without matches in source repos
-	console.log(varNames);
-
+	// Report the vars for which we found no matches in any other repo
+	// console.log(varNames);
+	const sortedKeys = Object.keys(varNames).sort();
+	// Start table
+	console.log('{| class="wikitable sortable"');
+	console.log('! Param !! Filename !! Component !! Status');
+	sortedKeys.forEach( varName => {
+		const fileNames = Array.from(varNames[varName]);
+		// Add table row
+		console.log('|-');
+		console.log('|<code>%s</code>', varName);
+		console.log('|<code>%s</code>', fileNames.join('</code>\n<code>'));
+		console.log('|');
+		console.log('|');
+	} );
+	// End table
+	console.log('|}');
 }());
