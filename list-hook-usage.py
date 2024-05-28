ed884fb0544c10ec764049e079cc9ccab8700db7
Wendy Quarshie <wquarshie@wikimedia.org>
2024-05-28 09:57:43 +0000

    adding support for skins Adding support for skins and Allow list-hook-usage to operate directly on /wmf-config/extension-list
    Bug: T355956
    Change-Id: I90a9141df46490e83d66fc028cac70bd984b0647

--- list-hook-usage.py
+++ list-hook-usage.py
@@ -1,23 +1,28 @@
+from re import search
+from argparse import ArgumentParser
+
 import requests
 import csv
 import sys
 
-def get_hooks(extension_name):
+
+def get_hooks(prefix, name):
     try:
-        extension_url = f"https://raw.githubusercontent.com/wikimedia/mediawiki-extensions-{extension_name}/master/extension.json"
-        response = requests.get(extension_url)
+        url = f"https://raw.githubusercontent.com/wikimedia/mediawiki-{prefix}s-{name}/master/{prefix}.json"
+        response = requests.get(url)
         response.raise_for_status()  # Raise an exception for bad HTTP responses
         data = response.json()
 
         # Extract hook names from the "Hooks" map
         hooks = data.get("Hooks", {})
 
-        return [(extension_name, hook) for hook in hooks.keys()]
+        return [(name, hook) for hook in hooks.keys()]
     except requests.exceptions.RequestException as err:
         print(f"Request Exception: {err}")
     return None
 
-def process_extensions(input_file, output_file):
+
+def process_extensions(input_file, output_file, is_skin):
     infile = sys.stdin if input_file == '-' else open(input_file, 'r')
     outfile = sys.stdout if output_file == '-' else open(output_file, 'w', newline='')
 
@@ -25,14 +30,38 @@ def process_extensions(input_file, output_file):
         csv_writer = csv.writer(outfile)
         csv_writer.writerow(['Extension Name', 'Hook Name'])
 
-        extension_names = [line.strip() for line in infile if line.strip() and not line.startswith("#")]
+        extension_names = []
+        for line in infile:
+            line = line.strip()
+            if line and not line.startswith("#"):
+                # Extract extension/skin name from the given format or use the line directly
+                match_extension = search(r'\$IP/extensions/([^/]+)/extension\.json', line)
+                match_skin = search(r'\$IP/skins/([^/]+)/skin\.json', line)
 
-        for extension_name in extension_names:
-            # Print status message if stdout not expected
+                if match_extension:
+                    if not is_skin:
+                        name = match_extension.group(1)
+                        extension_names.append(('extension', name))
+                elif match_skin:
+                    name = match_skin.group(1)
+                    extension_names.append(('skin', name))
+                else:
+                    parts = line.split(':')
+                    if len(parts) == 2 and parts[0] in ['extension', 'skin']:
+                        if parts[0] == 'extension' and not is_skin:
+                            extension_names.append((parts[0], parts[1]))
+                        elif parts[0] == 'skin':
+                            extension_names.append((parts[0], parts[1]))
+                    else:
+                        name = line
+                        determined_prefix = 'skin' if is_skin else 'extension'
+                        extension_names.append((determined_prefix, name))
+
+        for prefix, name in extension_names:
             if outfile != sys.stdout:
-                print(f"Fetching hook usage for {extension_name}")
+                print(f"Fetching hook usage for {prefix}:{name}")
 
-            hooks = get_hooks(extension_name)
+            hooks = get_hooks(prefix, name)
 
             if hooks is None:
                 continue
@@ -45,10 +74,12 @@ def process_extensions(input_file, output_file):
         if outfile != sys.stdout:
             outfile.close()
 
+
 if __name__ == "__main__":
-    if len(sys.argv) != 3:
-        print("Usage: python list-hook-usage.py input_file output_file")
-    else:
-        input_file_path = sys.argv[1]
-        output_file_path = sys.argv[2]
-        process_extensions(input_file_path, output_file_path)
+    parser = ArgumentParser(description='List hook usage for MediaWiki extensions and skins.')
+    parser.add_argument('input_file', help='Path to the input file or "-" for stdin')
+    parser.add_argument('output_file', help='Path to the output file or "-" for stdout')
+    parser.add_argument('--skins', action='store_true', help='Look for skins instead of extensions')
+
+    args = parser.parse_args()
+    process_extensions(args.input_file, args.output_file, args.skins)
